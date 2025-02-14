9ad5a3561497cff586124816e13e2a227b7d2db5
qkvkql <15281061897@163.com>
2025-02-14 23:39:41 +0800

    b

--- 气象/NOAA/noaa.js
+++ 气象/NOAA/noaa.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+/*
 //从html页面获取并操作数据
 const app = express(); //使用express module创建新的app
 app.set('view engine', 'ejs'); //决定使用ejs渲染模板
@@ -21,12 +22,21 @@ app.post("/",
         res.render('template', { addition: result });
     });
 app.listen(3000, function(){console.log("server is running on port 3000");})
+*/
 
-let config = {
-    stationNumber: '31338', //站号n1，后面还有n2,n3
-    sortFirst: 'low', //最低、最高
-    target: 'min', //夜温、均温、昼温
-    showNumber: 50, //显示多少个结果
+const config = {
+    stationNumber: '44221', //站号n1，后面还有n2,n3
+    sortFirst: 'low', //最低、最高( low or high )
+    target: 'min', //夜温、均温、昼温( min, max or avg )
+    showNumber: 20, //显示多少个结果
+
+    month: {
+        ifShowAll: true,
+        monthNumber: 1
+    },
+    yearStart: '1991',
+    yearEnd: '2020',
+
     n2: '0',
     n3: '99999',
     noaaLocation: 'D:/NOAA Data/', //NOAA csv文件根目录
@@ -44,6 +54,8 @@ let date = new Date().toISOString();
 let dateAndTimeArr = date.split('T');
 let dateArr = dateAndTimeArr[0].split('-');
 let y2 = dateArr[0];
+
+let tools = new Tools();
 
 //收集所有目标csv文件地址到数组paths
 let prePaths = [];
@@ -65,12 +77,12 @@ prePaths.forEach((v) => {
 //批量异步依次读取csv文件内容
 let sourceStr = '';
 readMultipleFiles(new Set(paths), 'utf8').subscribe({
-    next(result) {
+    next(result){
         sourceStr += result.contents;
     },
-    complete() {
+    complete(){
         let result = new Result();
-        //result.showLowestList();
+        result.showLowestList();
     }
 });
 
@@ -78,10 +90,10 @@ readMultipleFiles(new Set(paths), 'utf8').subscribe({
 function Result(){
     //极端低温
     function showLowestList(){
-        let ins = new Csv(sourceStr);
-        let arrOfCsv = ins.getArrOfCsv();
+        let csv = new Csv(sourceStr);
+        let startArr = getStartArr(csv.getArrOfCsv());
 
-        let obj0 = arrOfCsv[0];
+        let obj0 = startArr[0];
         let station0 = obj0.STATION;
         let name0 = obj0.NAME;
         let elevation0 = obj0.ELEVATION;
@@ -90,18 +102,59 @@ function Result(){
         console.log('站名: ' + name0 + ';\n站号: ' + station0 + '\n海拔：' + elevation0 + '\n纬度：' + latitude0 + '\n经度：' + longitude0 + '\n');
 
         for(let i = 0; i < config.showNumber; i++){
-            let tempObj = arrOfCsv[i];
+            let tempObj = startArr[i];
             let date = tempObj.DATE;
-            let avg = TFC(tempObj.TEMP);
-            let min = TFC(tempObj.MIN);
-            let max = TFC(tempObj.MAX);
-            console.log(FN(i+1) + '\t' + date + '\tmin:' + min + ';\tavg:' + avg + ';\tmax:' + max);
+            let avg = tempObj.TEMP === undefined ? undefined : (tempObj.TEMP).toFixed(1);
+            let min = tempObj.MIN === undefined ? undefined : (tempObj.MIN).toFixed(1);
+            let max = tempObj.MAX === undefined ? undefined : (tempObj.MAX).toFixed(1);
+            console.log(tools.FN(i+1, config.showNumber) + '\t' + date + '\tmin:' + min + ';\tavg:' + avg + ';\tmax:' + max);
         }
     }
     this.showLowestList = showLowestList;
 }
 
-//将读取的多个文件内容sourceStr放入数组sourceArr中
+//获取修正后的数组。检查、优化原始数据组，输出不含明显错误数据、用摄氏度表示气温、气温数据类型是数字的、正式开始用于统计的数组
+function getStartArr(arr){
+    let newArr = [];
+    arr.forEach((v) => {
+        let tools = new Tools();
+        let obj = v; //把对象v暂时赋值给obj
+        //判断气温是否在正常范围，然后转换成摄氏度
+        //均温
+        if(tools.isValidTempF(v.TEMP)){
+            let avg = tools.TFC(v.TEMP);
+            obj.TEMP = avg;
+        }else{
+            obj.TEMP = undefined;
+        }
+        //低温
+        if(tools.isValidTempF(v.MIN)){
+            let min = tools.TFC(v.MIN);
+            obj.MIN = min;
+        }else{
+            obj.MIN = undefined;
+        }
+        //高温
+        if(tools.isValidTempF(v.MAX)){
+            let max = tools.TFC(v.MAX);
+            obj.MAX = max;
+        }else{
+            obj.MAX = undefined;
+        }
+        //露点温度
+        if(tools.isValidTempF(v.DEWP)){
+            let dewp = tools.TFC(v.DEWP);
+            obj.DEWP = dewp;
+        }else{
+            obj.DEWP = undefined;
+        }
+        //填装obj到新数组
+        newArr.push(obj);
+    });
+    return newArr;
+}
+
+//获取原始数组。将读取的多个文件内容sourceStr放入数组sourceArr中
 //CSV String to Array
 function Csv(str){
     let arrOfCsv = [];
@@ -122,7 +175,7 @@ function Csv(str){
         });
 
         return titleArr;
-    }
+    }    
 
     //非标题行
     //获取csv行
@@ -136,11 +189,20 @@ function Csv(str){
                     arrayOfRowCells.push(v.trim());
                 }
             });
+
+            let strOfMonthNumber = tools.FN(config.month.monthNumber, 12);
+            let tempRegExp = new RegExp('-' + strOfMonthNumber + '-', 'i');
             //如果不是某个csv文件的标题行
             if(arrayOfRowCells[0] !== 'STATION'){
                 let tempObj = {};
                 for(let i = 0; i < arrayOfRowCells.length; i++){
-                    tempObj[getArrOfTitles()[i]] = arrayOfRowCells[i];
+                    if(config.month.ifShowAll === false){
+                        if(tempRegExp.test(arrayOfRowCells[1])){
+                            tempObj[getArrOfTitles()[i]] = arrayOfRowCells[i];
+                        }
+                    }else{
+                        tempObj[getArrOfTitles()[i]] = arrayOfRowCells[i];
+                    }
                 }
                 arrOfCsv.push(tempObj);
             }
@@ -176,29 +238,58 @@ function Csv(str){
     this.getArrOfCsv = getArrOfCsv;
 }
 
-//气温单位华氏度转摄氏度
-function TFC(fv){
-    let cv = ((fv - 32) * (5/9)).toFixed(1);
-    return cv;
-}
-//序号位数不够时前面补零
-function FN(n){
-    let showNumber = config.showNumber;
-    let result = '';
-    let preStr = '';
-    let zeroStr = '0';
-    let numberMax = Math.pow(10, showNumber.toString().length);
-    let w1 = n.toString().length;
-    let w2 = numberMax.toString().length;
-    if(w1 < w2){
-        let diff = w2 - w1;
-        for(let i = 0; i < diff; i++){
-            preStr += zeroStr;
+//所有需要的各类简单工具函数
+function Tools(){
+    //判断气温记录是否在正常范围，正常范围：华氏度-166 ~ 158，也就是摄氏度-110 ~ 70
+    //判断华氏度str输入，华氏度正常气温范围：-166 ~ 158
+    function isValidTempF(fvStr){
+        let fv = Number(fvStr);
+        if(fv < -166 || fv > 158){
+            return false;
+        }else{
+            return true;
         }
-        result = preStr + n.toString();
-    }else{
-        result = n.toString();
     }
-    return result;
+    //判断摄氏度str输入，摄氏度正常气温范围：-110 ~ 70
+    function isValidTempC(cvStr){
+        let cv = Number(cvStr);
+        if(cv < -110 || cv > 70){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    //判断两个yyyy-mm-dd格式的日期是否相邻，统计连续天数的时候有用！
+
+    //序号位数不够时前面补零
+    function FN(n, maxNumber){
+        let result = '';
+        let preStr = '';
+        let zeroStr = '0';
+        let numberMax = Math.pow(10, maxNumber.toString().length);
+        let w1 = n.toString().length;
+        let w2 = numberMax.toString().length;
+        if(w1 < w2){
+            let diff = w2 - w1;
+            for(let i = 1; i < diff; i++){
+                preStr += zeroStr;
+            }
+            result = preStr + n.toString();
+        }else{
+            result = n.toString();
+        }
+        return result;
+    }
+
+    //气温单位华氏度转摄氏度
+    function TFC(fvStr){
+        let cv = ((Number(fvStr) - 32) * (5/9));
+        return cv;
+    }
+
+    this.isValidTempF = isValidTempF;
+    this.isValidTempC = isValidTempC;
+    this.FN = FN;
+    this.TFC = TFC;
 }
-//判断两个yyyy-mm-dd格式的日期是否相邻，统计连续天数的时候有用！
