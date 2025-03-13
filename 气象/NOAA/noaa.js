4714d899e77aa259fe98f236b1ac8e63a498b837
qkvkql <15281061897@163.com>
2025-03-14 02:24:19 +0800

    b

--- 气象/NOAA/noaa.js
+++ 气象/NOAA/noaa.js
@@ -5,35 +5,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-/*
-//从html页面获取并操作数据
-const app = express(); //使用express module创建新的app
-app.set('view engine', 'ejs'); //决定使用ejs渲染模板
-app.set("views", __dirname); //设置.ejs html模板文件存放在哪个目录
-app.use(bodyParser.urlencoded({extended: true})); //准备工作
-//从哪个html页面获取
-app.get("/",function(req, res){res.sendFile(__dirname + "/index.html");});
-//获取什么，然后怎么操作
-app.post("/",
-    function(req, res){
-        let num1 = Number(req.body.num1);
-        let num2 = Number(req.body.num2);
-        let result = num1 + num2;
-        res.render('template', { addition: result });
-    });
-app.listen(3000, function(){console.log("server is running on port 3000");})
-*/
-
+//配置对象，用于设置筛选条件
 const config = {
-    stationNumber: '51076', //站号n1，后面还有n2,n3
+    stationNumber: '31338', //站号n1，后面还有n2,n3
 
-    yearStart: '1929', //起始年份，默认1929
-    yearEnd: '', //结束年份，为空则是当前年份
-    month: 0, //0 = 全年, 1 = 一月, ... , 12 = 十二月
+    dateStart: '1961-01-01',
+    dateEnd: '1990-12-31',
+    //yearStart: '1929', //起始年份，默认1929
+    //yearEnd: '', //结束年份，为空则是当前年份
+    month: 1, //0 = 全年, 1 = 一月, ... , 12 = 十二月
     
     order: 'asc', //asc(从小到大、从低到高，从早到晚), desc
     item: 'min', //date, min, avg, max
     showNumber: 10, //显示多少个结果
+
+    monthValidCount: 20, //某个月的avg/min/max至少有20个正常记录，才算一个有效avg/min/max月,才会计算有参考价值的月度均值
+    consec: { 
+        direction: 0, //连续记录考察方向，0代表小于等于临界值，1代表大于等于临界值
+        temperature: '-30' //连续记录临界值
+    },
     
     consoleAll: 0, //是否console所有筛选过、纳入考察的数组。0 = 否，1 = 是
 
@@ -55,21 +45,40 @@ const config = {
     ]
 }
 
+//初始化工具对象
+let tools = new Tools();
+
+/*
+//从html页面获取并操作数据
+const app = express(); //使用express module创建新的app
+app.set('view engine', 'ejs'); //决定使用ejs渲染模板
+app.set("views", __dirname); //设置.ejs html模板文件存放在哪个目录
+app.use(bodyParser.urlencoded({extended: true})); //准备工作
+//从哪个html页面获取
+app.get("/",function(req, res){res.sendFile(__dirname + "/index.html");});
+//获取什么，然后怎么操作
+app.post("/",
+    function(req, res){
+        let num1 = Number(req.body.num1);
+        let num2 = Number(req.body.num2);
+        let result = num1 + num2;
+        res.render('template', { addition: result });
+    });
+app.listen(3000, function(){console.log("server is running on port 3000");})
+*/
+
 //目标信息：站号、起止年份(最早开始年份：1929)
 let n1 = config.stationNumber; //data type: string
 let n2 = config.n2;
 let n3 = config.n3;
 let n = n1 + n2 + n3;
-//起始年份
-let y1 = config.yearStart;
-//结束年份
-let date = new Date().toISOString();
-let dateAndTimeArr = date.split('T');
-let dateArr = dateAndTimeArr[0].split('-');
-let y2 = config.yearEnd.length === 4 ? config.yearEnd : dateArr[0];
-
-//初始化工具对象
-let tools = new Tools();
+//起始日期
+let y1 = config.dateStart.split('-')[0];
+let d1 = config.dateStart;
+//let y1 = config.yearStart;
+//结束日期
+let y2 = config.dateEnd.split('-')[0].length === 4 ? config.dateEnd.split('-')[0] : tools.getCurrentDate().YEAR;
+let d2 = config.dateEnd.length === 10 ? config.dateEnd : tools.getCurrentDate().YMD;
 
 //收集所有目标csv文件地址到数组paths
 let prePaths = [];
@@ -108,12 +117,12 @@ readMultipleFiles(new Set(paths), 'utf8').subscribe({
 //显示结果
 function Result(){
     this.consoleResult = consoleResult;
-    //极端低温
+    //console打印结果
     function consoleResult(){
         let csv = new Csv(sourceStr);
-        let startArr = csv.getSortedArrOfCsv().arr; //getArrOfSelectedColumns(csv.getSortedArrOfCsv().arr);
+        let startObj = csv.getSortedArrOfCsv();
 
-        let obj0 = startArr[0];
+        let obj0 = startObj.arr[0];
         //let station0 = obj0.STATION;
         let name = obj0['NAME'];
         let elev = obj0['ELEVATION'];
@@ -121,7 +130,8 @@ function Result(){
         let lon = obj0['LONGITUDE'];
         
         //这里整理一下年份console格式，都是简单细活，没啥技术含量
-        console.log('\n(' + y1 + '-' + y2 + ') | 共 ' + fileCount + ' 年、(筛)' + startArr.length + '/(总)' + csv.getSortedArrOfCsv().totalDaysBeforeSort + ' 天');
+        console.log('\nDATE RANGE: [ ' + d1 + ', ' + d2 + ' ]');
+        console.log('YEAR(S) INVOLVED: ' + + fileCount);
         let strA = ''; //输出年，多行str
         let cnt = 0; //计数
         arrOfRecordedYears.forEach((v) => {
@@ -134,11 +144,15 @@ function Result(){
             }
         });
         console.log(strA);
-        console.log('\n站号 ' + config.stationNumber + ' | 站名 ' + name);
-        console.log('海拔 ' + elev + '(m) | 坐标 (' + lat + ', ' + lon + ')\n');
+        console.log('DAY(S) RECORDED: ' + startObj.totalDaysBeforeSort);
+        console.log('DAY(S) SELECTED: ' + startObj.arr.length);
+        //站点信息
+        console.log('\n' + config.stationNumber + ' | ' + name);
+        console.log(elev + '(m) | ( ' + lat + ', ' + lon + ' )\n');
 
         for(let i = 0; i < config.showNumber; i++){
-            let tempObj = startArr[i];
+            if(!startObj.arr[i]){ break; }//如果筛选出的天数小于预定展示天数，直接结束循环
+            let tempObj = startObj.arr[i];
             let date = tempObj.DATE;
             let min = tempObj['MIN'] === undefined ? undefined : (tempObj['MIN']);
             let avg = tempObj['TEMP'] === undefined ? undefined : (tempObj['TEMP']);
@@ -148,24 +162,10 @@ function Result(){
         }
         
         if(config.consoleAll){
-            console.table(startArr);
+            console.table(startObj.arr);
         }
     }
 }
-
-//选择需要的列比如气温相关列，舍弃不需要的列比如风速、降水等
-/* function getArrOfSelectedColumns(arr){
-    let newArr = [];
-    arr.forEach((v) => {
-        let obj = {};
-        config.options.forEach((vo) => {
-            obj[vo] = v[vo];
-        });
-
-        newArr.push(obj);
-    });
-    return newArr;
-} */
 
 //获取原始数组。将读取的多个文件内容sourceStr放入数组sourceArr中
 //CSV String to Array
@@ -201,6 +201,21 @@ function Csv(str){
     //获取csv行，尽兴以下处理：检查异常值(空、错误值等)、转换温度单位(华氏转摄氏)、限制数值小数点后位数、还有最复杂的排序(根据设置)
     function getSortedArrOfCsv(){
         let totalDays = 0;
+        
+        //以下统计功能的对象详细格式参考.txt文档，这些对象总计100多个细分属性
+        let stat_YM = {}; //某年某月，比如'1960-01'
+        
+        let stat_M = {}; //某个月份，比如'01'
+        let arrOfMonthStr = []; //月份字符数组，12个元素
+        for(let i=1; i<=12; i++){ arrOfMonthStr.push(tools.FN(i, 12)); }
+        arrOfMonthStr.forEach((v) => {
+            stat_M[v] = {};
+        });
+
+        let stat_Y = {}; //某年，比如'1969'
+        let stat_MD = {}; //某月某日，比如'01-20'
+        let stat_Consec = {}; //连续记录
+
         rowArr.forEach((v) => {
             let strOfRows = v.trim();
             let arrayOfRowCells = [];
@@ -212,7 +227,7 @@ function Csv(str){
             });
 
             let strOfMonthNumber = tools.FN(config.month, 12);
-            let tempRegExp = new RegExp('-' + strOfMonthNumber + '-', 'i');
+            let monthRegExp = new RegExp('-' + strOfMonthNumber + '-', 'i');
             
             //如果不是某个csv文件的标题行，达到了剔除标题行的目的
             if(arrayOfRowCells[0] !== 'STATION'){
@@ -220,13 +235,19 @@ function Csv(str){
                 let lenOfRowArr = arrayOfRowCells.length;
                 let tempObj = {};
                 if(config.month !== 0){//0 = 全年，非0则是筛选到单月
-                    if(tempRegExp.test( arrayOfRowCells[1] )){
+                    if(monthRegExp.test( arrayOfRowCells[1] )){
                         for(let i=0; i<lenOfRowArr; i++){
                             tempObj[getArrOfTitles()[i]] = arrayOfRowCells[i];
                         }
                         arrOfCsv.push(tempObj);
                     }
                 }else{ //全年
+                    let tc1 = Date.parse(arrayOfRowCells[1]) >= Date.parse(d1);
+                    let tc2 = Date.parse(arrayOfRowCells[1]) <= Date.parse(d2);
+                    
+                    //如果日期不在设置范围内，则忽略本次循环，forEach函数忽略本次循环不用continue，用return
+                    if(!tc1 || !tc2){ return; }
+                    //如果日期在设置范围内，则正常执行本次循环内容
                     for(let i=0; i<lenOfRowArr; i++){
                         tempObj[getArrOfTitles()[i]] = arrayOfRowCells[i];
                     }
@@ -252,6 +273,92 @@ function Csv(str){
             v['MAX'] = tools.isValidTempF(v['MAX']) ? tools.TFC(v['MAX']).toFixed(1) : undefined;
         });
 
+
+        /************************ START START ************************/
+        /************************ 超复杂统计部分 ************************/
+        /************************ START START ************************/
+        /************************ START START ************************/
+        //临时存储对象
+        let vObj_YM = {}; //某年某月
+        let vObj_M = {}; //某个月份
+        let vObj_Y = {}; //某年
+        let vObj_MD = {}; //某月某日
+        let vObj_consec = {}; //连续记录
+        let consec_count = 0;
+        let consec_isNewConsec = true;
+        let consec_dateStart = '';
+        let consec_dateEnd = '';
+
+        newArr.forEach((v) => {
+            let temp_YMD_Arr = v['DATE'].split('-');
+            
+            //日期片段
+            let temp_YM_Str = temp_YMD_Arr[0] + '-' + temp_YMD_Arr[1];
+            let temp_M_Str = temp_YMD_Arr[1];
+            let temp_Y_Str = temp_YMD_Arr[0];
+            let temp_MD_Str = temp_YMD_Arr[1] + '-' + temp_YMD_Arr[2];
+            
+            //某年某月
+            if(!stat_YM.hasOwnProperty(temp_YM_Str)){
+                vObj_YM = {};
+                vObj_YM.dayCount = 0;
+                vObj_YM.dayCountForMin = 0;
+                vObj_YM.dayCountForAvg = 0;
+                vObj_YM.dayCountForMax = 0;
+                vObj_YM.minArr = [];
+                vObj_YM.avgArr = [];
+                vObj_YM.maxArr = [];
+                vObj_YM.valid = {
+                    'avgForMin': undefined,
+                    'avg': undefined,
+                    'avgForMax': undefined
+                }
+            }
+            vObj_YM.dayCount += 1; //无论是否有undefined，日数都加1
+
+            if(v['MIN'] !== undefined){
+                vObj_YM.minArr.push( Number(v['MIN']) );
+                vObj_YM.dayCountForMin += 1;
+                vObj_YM.min = Math.min(...vObj_YM.minArr);
+                vObj_YM.avgForMin = tools.getAvgForNumberArr(vObj_YM.minArr);
+                vObj_YM.maxForMin = Math.max(...vObj_YM.minArr);
+                if(vObj_YM.minArr.length >= config.monthValidCount){vObj_YM.valid.avgForMin = tools.getAvgForNumberArr(vObj_YM.minArr)}
+            }
+            if(v['TEMP'] !== undefined){
+                vObj_YM.avgArr.push( Number(v['TEMP']) );
+                vObj_YM.dayCountForAvg += 1;
+                vObj_YM.minForAvg = Math.min(...vObj_YM.avgArr);
+                vObj_YM.avg = tools.getAvgForNumberArr(vObj_YM.avgArr);
+                vObj_YM.maxForAvg = Math.max(...vObj_YM.avgArr);
+                if(vObj_YM.avgArr.length >= config.monthValidCount){vObj_YM.valid.avg = tools.getAvgForNumberArr(vObj_YM.avgArr)}
+            }
+            if(v['MAX'] !== undefined){
+                vObj_YM.maxArr.push( Number(v['MAX']) );
+                vObj_YM.dayCountForMax += 1;
+                vObj_YM.minForMax = Math.min(...vObj_YM.maxArr);
+                vObj_YM.avgForMax = tools.getAvgForNumberArr(vObj_YM.maxArr);
+                vObj_YM.max = Math.max(...vObj_YM.maxArr);
+                if(vObj_YM.maxArr.length >= config.monthValidCount){vObj_YM.valid.avgForMax = tools.getAvgForNumberArr(vObj_YM.maxArr)}
+            }
+            stat_YM[temp_YM_Str] = vObj_YM; //完成一个键值对
+
+            //某个月份
+            if(!stat_M.hasOwnProperty(temp_M_Str)){ stat_M[temp_M_Str] = {}; }
+            
+            //某年
+            if(!stat_Y.hasOwnProperty(temp_Y_Str)){ stat_Y[temp_Y_Str] = {}; }
+            
+            //某月某日
+            if(!stat_MD.hasOwnProperty(temp_MD_Str)){ stat_MD[temp_MD_Str] = {}; }
+            
+            //连续记录
+        });
+        /************************ END END END ************************/
+        /************************ END END END ************************/
+        /************************ 超复杂统计部分 ************************/
+        /************************ END END END ************************/
+
+
         //根据config决定排序方式
         let ci = config.item;
         let co = config.order;
@@ -276,6 +383,14 @@ function Csv(str){
                 newArr.sort((a, b) => Date.parse(b.DATE) - Date.parse(a.DATE));
             }
         }
+
+        console.log(stat_YM);
+        /* let x = Object.keys(stat_YM);
+        x.sort((a, b) => {
+            return Date.parse(b) - Date.parse(a);
+        });
+        console.log(x); */
+        /************************ 这个return返回所有数据，量超大 ************************/
         return {
             'arr': newArr,
             'totalDaysBeforeSort': totalDays
@@ -289,6 +404,33 @@ function Csv(str){
 
 //所有需要的各类简单工具函数
 function Tools(){
+    //求均值
+    function getAvgForNumberArr(arr){
+        let sum = 0;
+        let len = arr.length;
+        arr.forEach((v) => {
+            sum += v;
+        });
+        return len > 0 ? (sum/len).toFixed(1) : undefined;
+    }
+    //获取2个日期相隔多少天
+    function getDateDiff(dateStr1, dateStr2){
+        let date1 = new Date(dateStr1);
+        let date2 = new Date(dateStr2);
+        let diffOfMilliSeconds = date2.getTime() - date1.getTime();
+        return Math.round(diffOfMilliSeconds/(1000 * 3600 * 24));
+    }
+    //获取当前日期YMD
+    function getCurrentDate(){
+        let date = new Date().toISOString();
+        let dateAndTimeArr = date.split('T');
+        let dateArr = dateAndTimeArr[0].split('-');
+        return {
+            'YEAR': dateArr[0],
+            'YMD': dateAndTimeArr
+        }
+    }
+    //在对象数组中，把包含undefined值的对象排到后面
     function sortUndefinedObj(a, b, order){
         if(a === undefined && b === undefined){
             return 0;
@@ -340,6 +482,9 @@ function Tools(){
         return cv;
     }
 
+    this.getAvgForNumberArr = getAvgForNumberArr;
+    this.getDateDiff = getDateDiff;
+    this.getCurrentDate = getCurrentDate;
     this.sortUndefinedObj = sortUndefinedObj;
     this.isValidTempF = isValidTempF;
     this.FN = FN;
