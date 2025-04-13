afb1aeae89387073eb853f0383617966197e6a72
qkvkql <15281061897@163.com>
2025-04-14 02:51:39 +0800

    a

--- 气象/今日最低气温/minToday.js
+++ 气象/今日最低气温/minToday.js
@@ -2,62 +2,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     fillTable();
 });
 
-let stations = [
-    ["卡丘格",30622],
-	["巴尔古津",30636],
-	["莫戈恰",30673],
-	["乌留皮诺",30781],
-	["埃基姆昌",31329],
-	["布鲁坎",31348],
-	["索菲斯克",31478],
-	["切昆达",31532],
-	["奥布卢奇耶",31702],
-	["克孜勒",36096],
-	["科什阿加奇",36259],
-	["埃尔津",36307],
-	["仁钦隆勃",44203],
-	["乌兰固木",44212],
-    ["巴彦特斯",44221],
-    ["车臣乌拉",44224],
-	["陶松臣格勒",44225],
-	["乌兰巴托",44292],
-	["漠河",50136],
-	["图里河",50434],
-	["海拉尔",50527],
-	["阿勒泰",51076],
-	["乌鲁木齐",51463],
-	["巴音布鲁克",51542],
-	["呼和浩特",53463],
-    ["武汉",57494]
-];
-let rp5Names = {
-    "30622": "Kachug",
-    "30636": "Barguzin",
-    "30673": "Mogocha",
-    "30781": "Uryupino",
-    "31329": "Ekimchan",
-    "31348": "Burukan",
-    "31478": "Sofiysk",
-    "31532": "Cekunda",
-    "31702": "Obluchye",
-    "36096": "Kyzyl",
-    "36259": "Kosh-Agach",
-    "36307": "Erzin",
-    "44203": "Rinchinlhumbe",
-    "44212": "Ulaangom",
-    "44221": "Gandan_Huryee",
-    "44224": "Tsetsen-Uul",
-    "44225": "Tosontsengel",
-    "44292": "Ulan_Bator",
-    "50136": "Xilinji",
-    "50434": "Tulihe",
-    "50527": "Zhengyang",
-    "51076": "Aletai",
-    "51463": "Urumqi",
-    "51542": "Baianbulak",
-    "53463": "Hohhot",
-    "57494": "Wuhan_(airport)"
-};
+let arrOfStations = [{"USAF":44212,"NAME":"乌兰固木","urlSuffOfRp5":"Ulaangom"},{"USAF":44221,"NAME":"巴彦特斯","urlSuffOfRp5":"Gandan_Huryee"},{"USAF":44224,"NAME":"车臣乌拉","urlSuffOfRp5":"Tsetsen-Uul"},{"USAF":44225,"NAME":"陶松臣格勒","urlSuffOfRp5":"Tosontsengel"},{"USAF":44203,"NAME":"仁钦隆勃","urlSuffOfRp5":"Rinchinlhumbe"},{"USAF":44292,"NAME":"乌兰巴托","urlSuffOfRp5":"Ulan_Bator"},{"USAF":54342,"NAME":"沈阳","urlSuffOfRp5":"Shenyang"},{"USAF":54161,"NAME":"长春","urlSuffOfRp5":"Changchun_(airport)"},{"USAF":54273,"NAME":"桦甸","urlSuffOfRp5":"Huadian"},{"USAF":50953,"NAME":"哈尔滨","urlSuffOfRp5":"Harbin_(airport)"},{"USAF":50353,"NAME":"呼玛","urlSuffOfRp5":"Huma"},{"USAF":50136,"NAME":"漠河","urlSuffOfRp5":"Xilinji"},{"USAF":50434,"NAME":"图里河","urlSuffOfRp5":"Tulihe"},{"USAF":50527,"NAME":"海拉尔","urlSuffOfRp5":"Zhengyang"},{"USAF":50727,"NAME":"阿尔山","urlSuffOfRp5":"Arxan"},{"USAF":52908,"NAME":"五道梁","urlSuffOfRp5":"Udaolyan"},{"USAF":52323,"NAME":"马鬃山","urlSuffOfRp5":"Mazong_Shan"},{"USAF":51076,"NAME":"阿勒泰","urlSuffOfRp5":"Aletai"},{"USAF":51573,"NAME":"吐鲁番","urlSuffOfRp5":"Turfan"},{"USAF":51463,"NAME":"乌鲁木齐","urlSuffOfRp5":"Urumqi"},{"USAF":51542,"NAME":"巴音布鲁克","urlSuffOfRp5":"Baianbulak"},{"USAF":53463,"NAME":"呼和浩特","urlSuffOfRp5":"Hohhot"},{"USAF":54511,"NAME":"北京","urlSuffOfRp5":"Beijing,_Peking"},{"USAF":53614,"NAME":"银川","urlSuffOfRp5":"Yinchuan"},{"USAF":53698,"NAME":"石家庄","urlSuffOfRp5":"Shijiazhuang"},{"USAF":53772,"NAME":"太原","urlSuffOfRp5":"Taiyuan_(airport)"},{"USAF":52866,"NAME":"西宁","urlSuffOfRp5":"Xining"},{"USAF":54823,"NAME":"济南","urlSuffOfRp5":"Jinan"},{"USAF":57083,"NAME":"郑州","urlSuffOfRp5":"Zhengzhou"},{"USAF":57131,"NAME":"西安(泾河)","urlSuffOfRp5":"Xi'an"},{"USAF":57245,"NAME":"安康","urlSuffOfRp5":"Ankang"},{"USAF":57687,"NAME":"长沙","urlSuffOfRp5":"Xingsha"},{"USAF":58606,"NAME":"南昌","urlSuffOfRp5":"Liantang"},{"USAF":57494,"NAME":"武汉","urlSuffOfRp5":"Wuhan_(airport)"},{"USAF":58321,"NAME":"合肥","urlSuffOfRp5":"Hefei,_Liugang_(airport)"},{"USAF":58238,"NAME":"南京","urlSuffOfRp5":"Nanjing_(airport)"},{"USAF":58457,"NAME":"杭州","urlSuffOfRp5":"Hangzhou_(airport)"},{"USAF":58646,"NAME":"丽水","urlSuffOfRp5":"Lishui"},{"USAF":58847,"NAME":"福州","urlSuffOfRp5":"Fuzhou"},{"USAF":59287,"NAME":"广州","urlSuffOfRp5":"Guangzhou_(airport)"},{"USAF":59948,"NAME":"三亚","urlSuffOfRp5":"Sanya_(weather_station)"},{"USAF":59838,"NAME":"东方","urlSuffOfRp5":"Dongfang"},{"USAF":59431,"NAME":"南宁","urlSuffOfRp5":"Nanning_(airport)"},{"USAF":56966,"NAME":"元江","urlSuffOfRp5":"Lijiang"},{"USAF":56778,"NAME":"昆明","urlSuffOfRp5":"Kunming"},{"USAF":57816,"NAME":"贵阳","urlSuffOfRp5":"Guiyang"},{"USAF":57516,"NAME":"重庆(沙坪坝)","urlSuffOfRp5":"Chongqing,_Jiulongpo_(airport)"},{"USAF":57503,"NAME":"内江","urlSuffOfRp5":"Gaoqiao,_Neijiang"},{"USAF":56187,"NAME":"成都","urlSuffOfRp5":"Chengdu_(AWS)"},{"USAF":55591,"NAME":"拉萨","urlSuffOfRp5":"Lhasa"},{"USAF":31702,"NAME":"奥布卢奇耶","urlSuffOfRp5":"Obluchye"},{"USAF":31532,"NAME":"切昆达","urlSuffOfRp5":"Cekunda"},{"USAF":31478,"NAME":"索菲斯克","urlSuffOfRp5":"Sofiysk"},{"USAF":31329,"NAME":"埃基姆昌","urlSuffOfRp5":"Ekimchan"},{"USAF":31348,"NAME":"布鲁坎","urlSuffOfRp5":"Burukan"},{"USAF":30673,"NAME":"莫戈恰","urlSuffOfRp5":"Mogocha"},{"USAF":30664,"NAME":"通戈科琴","urlSuffOfRp5":"Tungokochen"},{"USAF":30636,"NAME":"巴尔古津","urlSuffOfRp5":"Barguzin"},{"USAF":30622,"NAME":"卡丘格","urlSuffOfRp5":"Kachug"},{"USAF":36104,"NAME":"萨雷格谢普","urlSuffOfRp5":"Saryg-Sep"},{"USAF":36096,"NAME":"克孜勒","urlSuffOfRp5":"Kyzyl"},{"USAF":36307,"NAME":"埃尔津","urlSuffOfRp5":"Erzin"},{"USAF":36259,"NAME":"科什阿加奇","urlSuffOfRp5":"Kosh-Agach"},{"USAF":31137,"NAME":"Toko","urlSuffOfRp5":"Toko"},{"USAF":24688,"NAME":"奥伊米亚康","urlSuffOfRp5":"Oymyakon"},{"USAF":30781,"NAME":"乌留皮诺","urlSuffOfRp5":"Uryupino"},{"USAF":38875,"NAME":"喀拉库勒","urlSuffOfRp5":"Karakul,_Tajikistan"},{"USAF":38358,"NAME":"苏萨梅尔","urlSuffOfRp5":"Suusamyr"},{"USAF":36535,"NAME":"科克佩克特","urlSuffOfRp5":"Kokpekty"},{"USAF":47005,"NAME":"三池渊","urlSuffOfRp5":"Samjiyon"}];
 
 let date = new Date().toISOString();
 let dateAndTimeArr = date.split('T');
@@ -78,7 +23,7 @@ function getHour(){
 
 function fillTable(){
     let tb = document.getElementById('tb');
-    let count = stations.length;
+    let count = arrOfStations.length;
 
     for(let i=0; i<count; i++){
         let tr = document.createElement('tr');
@@ -92,23 +37,15 @@ function fillTable(){
 
         //第2列：站号
         let td1 = document.createElement('td');
-        td1.innerText = stations[i][1].toString();
+        td1.innerText = arrOfStations[i].USAF.toString();
         tr.appendChild(td1);
 
         //第3列：站名
         let td2 = document.createElement('td');
-        td2.innerText = stations[i][0];
+        td2.innerText = arrOfStations[i].NAME;
         tr.appendChild(td2);
 
-        //第4列：Ogimet逐日(UTC时间不同，结果也不同)
-        let td3 = document.createElement('td');
-        tr.appendChild(td3);
-        let btn1 = document.createElement('button');
-        btn1.innerText = 'Ogimet逐日';
-        td3.appendChild(btn1);
-        td3.onclick = () => {window.open(getOgimetUrlD(i), '_blank')};
-
-        //第5列：Ogimet逐时(UTC时间不同，显示截止小时数也不同)
+        //Ogimet逐时(UTC时间不同，显示截止小时数也不同)
         let td4 = document.createElement('td');
         tr.appendChild(td4);
         let btn2 = document.createElement('button');
@@ -128,45 +65,53 @@ function fillTable(){
         td4.appendChild(btn2);
         td4.onclick = () => {window.open(getOgimetUrlH(i), '_blank')};
 
-        //第6列：Pogodailimat逐日。注意这个页面有预设时区，每日起始时间不一定是北京时间的00:00或02:00，虽然高低温逻辑是对的(相比ogimet的逐日页面)
-        let td5 = document.createElement('td');
-        tr.appendChild(td5);
-        let btn3 = document.createElement('button');
-        btn3.innerText = 'Pogodailimat逐日';
-        td5.appendChild(btn3);
-        td5.onclick = () => {window.open(getPogodailimatUrlD(i), '_blank')};
-
-        //第7列：Pogodailimat逐时
-        let td6 = document.createElement('td');
-        tr.appendChild(td6);
-        let btn4 = document.createElement('button');
-        btn4.innerText = 'Pogodailimat逐时';
-        td6.appendChild(btn4);
-        td6.onclick = () => {window.open(getPogodailimatUrlH(i), '_blank')};
-
-        //第8列：Pogodailimat气候
-        let td7 = document.createElement('td');
-        tr.appendChild(td7);
-        let btn5 = document.createElement('button');
-        btn5.innerText = 'Pogodailimat气候';
-        td7.appendChild(btn5);
-        td7.onclick = () => {window.open(getPogodailimatUrlClimate(i), '_blank')};
-
-        //第9列：Pogodailimat月均
-        let td8 = document.createElement('td');
-        tr.appendChild(td8);
-        let btn6 = document.createElement('button');
-        btn6.innerText = 'Pogodailimat月均';
-        td8.appendChild(btn6);
-        td8.onclick = () => {window.open(getPogodailimatUrlMonthAvg(i), '_blank')};
-
-        //第10列：Pogodailimat月均
+        //rp5逐时
         let td9 = document.createElement('td');
         tr.appendChild(td9);
         let btn7 = document.createElement('button');
         btn7.innerText = 'rp5逐时';
         td9.appendChild(btn7);
         td9.onclick = () => {window.open(getRp5UrlH(i), '_blank')};
+
+        //Pogodaiklimat月均
+        let td8 = document.createElement('td');
+        tr.appendChild(td8);
+        let btn6 = document.createElement('button');
+        btn6.innerText = 'Pogodaiklimat月均';
+        td8.appendChild(btn6);
+        td8.onclick = () => {window.open(getPogodaiklimatUrlMonthAvg(i), '_blank')};
+
+        //Pogodaiklimat气候
+        let td7 = document.createElement('td');
+        tr.appendChild(td7);
+        let btn5 = document.createElement('button');
+        btn5.innerText = 'Pogodaiklimat气候';
+        td7.appendChild(btn5);
+        td7.onclick = () => {window.open(getPogodaiklimatUrlClimate(i), '_blank')};
+
+        //Ogimet逐日(UTC时间不同，结果也不同)
+        let td3 = document.createElement('td');
+        tr.appendChild(td3);
+        let btn1 = document.createElement('button');
+        btn1.innerText = 'Ogimet逐日';
+        td3.appendChild(btn1);
+        td3.onclick = () => {window.open(getOgimetUrlD(i), '_blank')};
+
+        //Pogodaiklimat逐日。注意这个页面有预设时区，每日起始时间不一定是北京时间的00:00或02:00，虽然高低温逻辑是对的(相比ogimet的逐日页面)
+        let td5 = document.createElement('td');
+        tr.appendChild(td5);
+        let btn3 = document.createElement('button');
+        btn3.innerText = 'Pogodaiklimat逐日';
+        td5.appendChild(btn3);
+        td5.onclick = () => {window.open(getPogodaiklimatUrlD(i), '_blank')};
+
+        //Pogodaiklimat逐时
+        let td6 = document.createElement('td');
+        tr.appendChild(td6);
+        let btn4 = document.createElement('button');
+        btn4.innerText = 'Pogodaiklimat逐时';
+        td6.appendChild(btn4);
+        td6.onclick = () => {window.open(getPogodaiklimatUrlH(i), '_blank')};
 
         setBtnStyle([btn1, btn2, btn3, btn4, btn5, btn6, btn7]);
     }
@@ -192,33 +137,33 @@ function setBtnStyle(btnArr){
 }
 
 function getOgimetUrlD(c){
-    let url = "https://ogimet.com/cgi-bin/gsynres?ind=" + stations[c][1].toString() + "&ord=REV&enviar=Ver&ndays=30&ano=" + y + "&mes="
+    let url = "https://ogimet.com/cgi-bin/gsynres?ind=" + arrOfStations[c].USAF.toString() + "&ord=REV&enviar=Ver&ndays=30&ano=" + y + "&mes="
 + m + "&day=" + d + "&hora=" + getHour();
     return url;
 }
 function getOgimetUrlH(c){
-    let url = "https://ogimet.com/cgi-bin/gsynres?ind=" + stations[c][1].toString() + "&decoded=yes&ndays=7&ano=" + y + "&mes="
+    let url = "https://ogimet.com/cgi-bin/gsynres?ind=" + arrOfStations[c].USAF.toString() + "&decoded=yes&ndays=7&ano=" + y + "&mes="
 + m + "&day=" + d + "&hora=" + getHour();
     return url;
 }
 
-function getPogodailimatUrlD(c){
-    let url = "http://www.pogodaiklimat.ru/monitor.php?id=" + stations[c][1].toString();
+function getPogodaiklimatUrlD(c){
+    let url = "http://www.pogodaiklimat.ru/monitor.php?id=" + arrOfStations[c].USAF.toString();
     return url;
 }
-function getPogodailimatUrlH(c){
-    let url = "http://www.pogodaiklimat.ru/weather.php?id=" + stations[c][1].toString();
+function getPogodaiklimatUrlH(c){
+    let url = "http://www.pogodaiklimat.ru/weather.php?id=" + arrOfStations[c].USAF.toString();
     return url;
 }
-function getPogodailimatUrlClimate(c){
-    let url = "http://www.pogodaiklimat.ru/climate.php?id=" + stations[c][1].toString();
+function getPogodaiklimatUrlClimate(c){
+    let url = "http://www.pogodaiklimat.ru/climate.php?id=" + arrOfStations[c].USAF.toString();
     return url;
 }
-function getPogodailimatUrlMonthAvg(c){
-    let url = "http://www.pogodaiklimat.ru/history/" + stations[c][1].toString() + ".htm";
+function getPogodaiklimatUrlMonthAvg(c){
+    let url = "http://www.pogodaiklimat.ru/history/" + arrOfStations[c].USAF.toString() + ".htm";
     return url;
 }
 function getRp5UrlH(c){
-    let url = "https://rp5.ru/Weather_archive_in_" + rp5Names[stations[c][1]];
+    let url = "https://rp5.ru/Weather_archive_in_" + arrOfStations[c].urlSuffOfRp5;
     return url;
 }
