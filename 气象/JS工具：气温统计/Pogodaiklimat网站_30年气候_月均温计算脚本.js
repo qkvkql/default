0bfc69aed48a48b08b631028a66cedc1b45f141f
qkvkql <15281061897@163.com>
2025-08-14 13:03:23 +0800

    b

--- 气象/JS工具：气温统计/Pogodaiklimat网站_30年气候_月均温计算脚本.js
+++ 气象/JS工具：气温统计/Pogodaiklimat网站_30年气候_月均温计算脚本.js
@@ -1,11 +1,11 @@
-let climateAvgOf_30_Years_startYear = 1961;
+let climateYearStart = 1961;
 
+let climateYears = 30;
 let monthsText = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月', '全年'];
 let objOfMonthlyDataForClimate = {};
-let resultObj = {};
+let resultArr = [];
 for(let i = 0; i < monthsText.length; i++){ //初始化两个对象，按特定格式
     objOfMonthlyDataForClimate[monthsText[i]] = [];
-    //resultObj[monthsText[i]] = -100;
 }
 
 let leftColumns = document.getElementsByClassName("chronicle-table-left-column")[0].getElementsByTagName('tr');
@@ -17,14 +17,17 @@ for(let lcy = 0; lcy < leftColumns.length; lcy++){
     let dataOfTheYear = dataOfYears[lcy].getElementsByTagName('td');
     for(let m = 0; m < dataOfTheYear.length; m++){
         let dataOfTheMonth = Number(dataOfTheYear[m].getElementsByTagName('font')[0].getElementsByTagName('font')[0].innerText.trim());
-        let startYear = Number(climateAvgOf_30_Years_startYear);
-        if( tempYear >= startYear && tempYear < (startYear + 30) ){
+        let startYear = Number(climateYearStart);
+        if( tempYear >= startYear && tempYear < (startYear + climateYears) ){
             if(dataOfTheMonth > -100 && dataOfTheMonth < 60){
                 objOfMonthlyDataForClimate[monthsText[m]].push(dataOfTheMonth);
             }
         }
     }
 }
+
+let tempStartY = Number(climateYearStart);
+let tempStr = '[ ' + tempStartY.toString() + '-' +(tempStartY + climateYears - 1).toString() + ' ] ';
 
 let arrOfObjK = Object.keys(objOfMonthlyDataForClimate);
 let arrOfObjV = Object.values(objOfMonthlyDataForClimate);
@@ -34,8 +37,11 @@ for(let i = 0; i < arrOfObjK.length; i++){
     for(let j=0; j<arrOfObjV[i].length; j++){
         tempSum += arrOfObjV[i][j];
     }
-    tempAvg = (tempSum/(arrOfObjV[i].length)).toFixed(1);
-    resultObj[arrOfObjK[i]] = tempAvg;
+    tempAvg = Number((tempSum/(arrOfObjV[i].length)).toFixed(1));
+    resultArr.push([]);
+    resultArr[i][tempStr + '气候'] = arrOfObjK[i];
+    resultArr[i]['平均气温'] = tempAvg;
+    resultArr[i]['年数'] = (arrOfObjV[i].length).toString() + ' / ' + climateYears.toString().trim();
 }
 console.log(objOfMonthlyDataForClimate);
-console.table(resultObj);
+console.table(resultArr);
