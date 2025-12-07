06982d8430064d479e91102bd36c299540e34e77
qkvkql <15281061897@163.com>
2025-12-08 01:19:12 +0800

    a

--- 气象/JS工具：气温统计/Mongolia_daily_3h_temp.js
+++ 气象/JS工具：气温统计/Mongolia_daily_3h_temp.js
@@ -10,34 +10,49 @@ let arrOfObj = [];
 for(let i=1; i<tbodys.length; i++){
     let tempObj = {};
     let tempTds = tbodys[i].getElementsByTagName('tr')[0].getElementsByTagName('td');
+    let tempValues = []
     for(let j=0; j<ths.length; j++){
         tempObj[arrOfThStrs[j]] = tempTds[j].innerText.toString().trim();
+        if(j > 3){
+            tempValues.push(Number(tempTds[j].innerText.toString().trim()))
+        }
     }
+
+    let tempSum = 0;
+    let total = tempValues.length; //数据个数
+    for(let j=0; j<total; j++){
+        tempSum += tempValues[j];
+    }
+    let avg = (tempSum/total).toFixed(2).toString();
+    tempObj['avg_of8'] = avg;
+    tempObj['total'] = total;
     arrOfObj.push(tempObj);
 }
 
 let resultStr = '';
+let tempK = Object.keys(arrOfObj[0]);
 //打印标题
-for(let i=0; i<ths.length; i++){
-    if(i < ths.length - 1){
-        resultStr += arrOfThStrs[i] + '\t';
+for(let i=0; i<tempK.length; i++){
+    if(i < tempK.length - 1){
+        resultStr += tempK[i] + '\t';
     }else{
-        resultStr += arrOfThStrs[i] + '\n';
+        resultStr += tempK[i] + '\n';
     }
 }
 //打印数据
 for(let i=0; i<arrOfObj.length; i++){
-    for(let j=0; j<ths.length; j++){
-        if(j < ths.length - 1){
-            resultStr += arrOfObj[i][arrOfThStrs[j]] + '\t';
+    for(let j=0; j<tempK.length; j++){
+        if(j < tempK.length - 1){
+            resultStr += arrOfObj[i][tempK[j]] + '\t';
         }else{
             if(i < arrOfObj.length - 1){
-                resultStr += arrOfObj[i][arrOfThStrs[j]] + '\n';
+                resultStr += arrOfObj[i][tempK[j]] + '\n';
             }else{
-                resultStr += arrOfObj[i][arrOfThStrs[j]];
+                resultStr += arrOfObj[i][tempK[j]];
             }
         }
     }
 }
 //打印结果
+//console.log(arrOfObj)
 console.log(resultStr);
